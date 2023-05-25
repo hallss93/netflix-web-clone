725131c725989e20208188b84acff639fe787083
Hallison Melo <hallison.pm@gmail.com>
2023-05-25 16:18:26 -0300

    Highlighted Component

---
+++ src/components/HighlightedMovie/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <img alt="Movie Poster" class="logo" src="@/assets/imgs/movie-poster.png" />
+</template>
+  
+<style lang="scss" scoped>
+img {
+    width: 100%;
+    height: auto;
+    left: 0px;
+    top: 0px;
+}
+</style>
