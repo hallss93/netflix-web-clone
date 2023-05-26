2f0de88b4d5b3ede9e8453df0d4b4c8a8025e70b
Hallison Melo <hallison.pm@gmail.com>
2023-05-26 10:13:39 -0300

    configure vue3-carousel

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import './assets/main.css'
+import './plugins/vue3-carousel'
 
 const app = createApp(App)
 
---
+++ src/plugins/vue3-carousel.ts
@@ -0,0 +1,1 @@
+import 'vue3-carousel/dist/carousel.css'
